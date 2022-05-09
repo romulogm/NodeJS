6e53978392b3bf266567a10e371e7060ac23f04e
romulogm <romulogm@gmail.com>
2022-05-09 18:57:32 -0300

    Scraping

--- Puppeteer/index.js
+++ Puppeteer/index.js
@@ -1,3 +1,5 @@
+// Acessar página individual do post, raspar e salvar dados, ir para a próxima página
+
 const puppeteer = require('puppeteer');
 
 (async () => {
@@ -5,19 +7,50 @@ const puppeteer = require('puppeteer');
 const browser = await puppeteer.launch({ headless: false });
 const page = await browser.newPage();
 await page.goto('https://lendoosclassicosluizruffato.blogspot.com');
+//
+await page.waitForSelector("p:nth-child(1) > span");
 
-let bookReview = document.querySelector("#post-body-2236296491671054818 > div:nth-child(2) > div:nth-child(19) > span:nth-child(1)");
+const title = await page.$eval(document.querySelector("p:nth-child(1) > span"), (title) => title.innerText); 
 
-const urls = await page.$$eval("#post-body-2236296491671054818 > div:nth-child(2) > div:nth-child(19) > span:nth-child(1)", (el) => {
-    return el.map((a) => a.getAttribute("href"));
-  });
+const image = await page.$eval("div.separator > a > img", (image) =>
+      image.getAttribute("src")
+    );
+
+const content = await page.$eval("span > p:nth-child(1) > span", el => el.innerText);     
 
 
-await console.log(bookReview);
+const post = {
+    title, 
+    image, 
+    content
+  };
+
+posts.push(post)
+
+/*
+let haveNext = false;
+
+do {
+  haveNext = false; 
+
+  const button_next_page = await page.$("#Blog1_blog-pager-older-link"); 
+
+  if (button_next_page) {
+    await Promise.all(
+      [
+        page.waitForNavigation(),  
+        page.$eval("#Blog1_blog-pager-older-link", e => e.click()) 
+      ]
+    );
+    haveNext = true; 
+  }
+} while (haveNext);
+
 await browser.close();
-
+*/
 })();
+
 // "#post-body-2236296491671054818 > div:nth-child(1)"
 // "document.querySelector("#post-body-2236296491671054818 > div:nth-child(2) > div:nth-child(19) > span:nth-child(1)")"
-//await page.screenshot({ path: 'screenshot2.png' });
 
+//http://www2.decom.ufop.br/terralab/tutorial-como-realizar-um-scraping-de-dados-em-um-website/
